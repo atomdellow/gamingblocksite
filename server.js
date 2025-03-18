edfa3e5a574c99d24a645a871600463a204561e3
Adam Dellow <atomdellow@gmail.com>
2025-03-18 15:21:24 -0400

    heroku changes for deploy, removed previous heroku remote, and added correct ones. Previous to this I adjusted the package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const path = require('path');
-
+// Gaming Blog Site Server
 // Load env vars
 dotenv.config();
 
