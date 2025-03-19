5ecb5e6b328023f65f480a4f20782a0334a055f6
Adam Dellow <atomdellow@gmail.com>
2025-03-19 19:24:20 -0400

    fix: update API configuration and add debugging

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://gbs-staging-3474ded0ac56.herokuapp.com/api'
-  : 'http://localhost:5000/api';
+const baseURL = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000/api'
+  : 'https://gbs-staging-3474ded0ac56.herokuapp.com/api';
 
 const api = axios.create({
   baseURL,
--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const app = express();
 
 // CORS configuration based on environment
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'staging'
-    ? ['https://gbs-staging-3474ded0ac56.herokuapp.com', 'http://localhost:5173']
-    : 'http://localhost:5173',
+  origin: ['https://gbs-staging-3474ded0ac56.herokuapp.com', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
