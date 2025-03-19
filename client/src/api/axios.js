b9c7c28256f587afeb2d658f4c46f61868414aa1
Adam Dellow <atomdellow@gmail.com>
2025-03-19 18:45:18 -0400

    Finalizing changes to connect to MongoDB

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,41 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? '/api' 
-  : 'http://localhost:5000/api'
+  ? 'https://gbs-staging-3474ded0ac56.herokuapp.com/api'
+  : 'http://localhost:5000/api';
 
-const instance = axios.create({
+const api = axios.create({
   baseURL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
-})
+});
 
-// Add a request interceptor to include the auth token in requests
-instance.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  error => {
-    return Promise.reject(error)
-  }
-)
-
-// Add a response interceptor to handle common errors
-instance.interceptors.response.use(
-  response => response.data,
-  error => {
-    // Handle token expiration
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token')
-      // Optional: Redirect to login page
-    }
-    return Promise.reject(error)
-  }
-)
-
-export default instance
+export default api;
